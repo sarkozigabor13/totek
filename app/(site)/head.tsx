0ce2708a6c24967f791140976178cd8e48bb2393
Gábor Sárközi <sarkozigabor13@gmail.com>
2025-10-06 15:18:45 +0200

    header

--- app/(site)/head.tsx
+++ app/(site)/head.tsx
@@ -5,6 +5,24 @@ export default function Head() {
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Built with Next.js and TypeScript" />
       <link rel="icon" href="/images/favicon.ico" />
+      {/* Android Chrome */}
+      <meta
+        name="theme-color"
+        media="(prefers-color-scheme: light)"
+        content="#1d8e4e"
+      />
+      <meta
+        name="theme-color"
+        media="(prefers-color-scheme: dark)"
+        content="#000000"
+      />
+
+      {/* iOS Safari */}
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta
+        name="apple-mobile-web-app-status-bar-style"
+        content="black-translucent"
+      />
     </>
   );
 }
